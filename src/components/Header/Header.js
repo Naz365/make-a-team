ac0aa4bbe78bf091113256c7eab0998f3681dc8a
Naz365 <nazmul.gub.cse@gmail.com>
2021-09-26 23:55:07 +0600

    added the header section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Make a Team </h1>
+      <div>
+          <p>
+              Make your team with GUCC
+          </p>
+          <h1>Total Budget: <span>500k</span></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
